546c636f62fb04ef0546c169ca30dd156ed238d2
jcparmar <93428181+jcparmar@users.noreply.github.com>
2021-11-18 15:00:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,91 @@
+var forest,rabbit,apple,orangeL,redL;
+var forestImg,rabbitImg,carrotImg,orangeImg,redImg;
+//var Hunger=0;
+
+function preload(){
+  forestImg = loadImage("forest1.jpg");
+  rabbitImg = loadImage("rabbit1.png");
+  appleImg = loadImage("apple.png");
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
+}
+
+
+function setup(){
+  
+  createCanvas(400,400);
+// Moving background
+forest=createSprite(200,200);
+forest.addImage(forestImg);
+
+
+//creating boy running
+rabbit = createSprite(160,340,20,20);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+function draw() {
+  background(0);
+  
+  //textSize (20)
+  //text ("Hunger : " + Hunger,280,20)
+
+  //if (rabbit.isTouching(apple)){
+   //Hunger= Hunger+1 
+  //}
+
+  // boy moving on Xaxis with mouse'
+  rabbit.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+  
+   drawSprites();
+   
+  
+
+
+ var select_sprites = Math.round(random(1,3));
+
+  
+   if (frameCount % 50 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     } else {
+       createRed();
+     }
+   }
+
+ 
+
+
+
+}
+
+function createApples() {
+apple = createSprite(random(50, 350),40, 10, 10);
+apple.addImage(appleImg);
+apple.scale=0.07;
+apple.velocityY = 3;
+apple.lifetime = 150;
+  
+}
+
+function createOrange() {
+orangeL = createSprite(random(50, 350),40, 10, 10);
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY = 3;
+orangeL.lifetime = 150;
+}
+
+function createRed() {
+redL = createSprite(random(50, 350),40, 10, 10);
+redL.addImage(redImg);
+redL.scale=0.06;
+  redL.velocityY = 3;
+  redL.lifetime = 150;
+}
